06a29a22f6830148183454601ee6b9edc4632dc4
AlgoDesk07\AlgoUser <esp2ar0@gmail.com>
2019-01-25 14:02:18 +0900

    1332번 문제 코드 약간 수정

--- src/main/java/baekjoon/problem_1332_ing.java
+++ src/main/java/baekjoon/problem_1332_ing.java
@@ -7,35 +7,14 @@ public class problem_1332_ing {
     public static void main(String[] args) {
         try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
-            int N = br.read() - '0';
-            br.read();
-            int V = br.read() - '0';
-            br.readLine();
-            String[] element = br.readLine().split(" ");
-            int[] P = new int[element.length];
-            for (int i = 0; i < element.length; i++) {
-                P[i] = Integer.parseInt(element[i]);
+            int N = br.read() - '0';    br.read();
+            int V = br.read() - '0';    br.readLine();
+            String[] elements = br.readLine().split(" ");
+            int[] P = new int[N];
+
+            for (int i = 0; i < elements.length; i++) {
+                P[i] = Integer.parseInt(elements[i]);
             } // input end
-
-
-
-
-            int max_interest = Integer.MIN_VALUE;
-            int min_interest = Integer.MAX_VALUE;
-
-            int count = 0;
-            int diff = max_interest - min_interest;
-            int temp;
-            for (int i = 0; i < N; i++) {
-                if(diff >= V) break;
-
-                max_interest = max_interest < P[i] ? P[i] : max_interest;
-                min_interest = min_interest < P[i] ? min_interest : P[i];
-                diff = max_interest - min_interest;
-
-                count++;
-            }
-
 
 
 
@@ -44,11 +23,5 @@ public class problem_1332_ing {
         }catch(IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public static int calculateDiff(int max_interest, int min_interest, int P) {
-        max_interest = max_interest < P ? P : max_interest;
-        min_interest = min_interest < P ? min_interest : P;
-        return  max_interest - min_interest;
     }
 }
